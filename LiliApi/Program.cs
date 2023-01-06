d1febfd98094215da3a1d90fa644ef9098d93395
ptrwilk <ptrwilk@outlook.com>
2023-01-06 14:08:14 +0100

    LL-2 - shutdown related endpoints

--- LiliApi/Program.cs
+++ LiliApi/Program.cs
@@ -1,6 +1,49 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Jira!");
+var isShutdownInProcess = false;
+
+app.MapPut("/shutdown", (double seconds) =>
+{
+    if(!isShutdownInProcess)
+    {
+        isShutdownInProcess = true;
+
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.Conflict("System shutdown is already in process");
+    }
+});
+
+app.MapPut("/shutdown/stop", () =>
+{
+    if (isShutdownInProcess)
+    {
+        isShutdownInProcess = false;
+
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.Conflict("System shutdown is already stopped");
+    }
+});
+
+app.MapGet("/shutdown/state", () =>
+{
+    if (isShutdownInProcess)
+    {
+        return Results.Ok(12);
+    }
+    else
+    {
+        return Results.Ok(-1);
+    }
+});
+
 
 app.Run();
